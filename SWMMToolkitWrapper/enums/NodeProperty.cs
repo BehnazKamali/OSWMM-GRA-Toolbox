5d9193252cac2b23ccb58338e9157494c0ed03cc
Behnaz <80097342+BehnazKamali@users.noreply.github.com>
2021-03-16 22:42:19 +0330

    Add files via upload

---
+++ SWMMToolkitWrapper/enums/NodeProperty.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWMMToolkitWrapper.enums
+{
+    public enum NodeProperty : byte
+    {
+        INVERTEL = 0,  /**< Invert Elevation */
+        FULLDEPTH = 1,  /**< Full Depth */
+        SURCHDEPTH = 2,  /**< Surcharge Depth */
+        PONDAREA = 3,  /**< Ponding Area */
+        INITDEPTH = 4,  /**< Initial Depth */
+    }
+}
